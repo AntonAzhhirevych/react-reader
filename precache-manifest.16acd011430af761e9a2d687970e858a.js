c27c022d2688a499fb64d86c8087dd49d0b2f29b
Anton Azhhirevych <azhhirevych943@gmail.com>
2019-08-02 16:18:22 +0300

    Updates

---
+++ precache-manifest.16acd011430af761e9a2d687970e858a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcb273588f594bb800d2d54ef99e80b1",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "3e84eed7293243bf4942",
+    "url": "/goit-react-hw-04-reader/static/css/main.125aa1a9.chunk.css"
+  },
+  {
+    "revision": "a0284b97086e693e1e7c",
+    "url": "/goit-react-hw-04-reader/static/js/2.bb2d39df.chunk.js"
+  },
+  {
+    "revision": "3e84eed7293243bf4942",
+    "url": "/goit-react-hw-04-reader/static/js/main.12a7363b.chunk.js"
+  },
+  {
+    "revision": "84bf89e08b01550b9b7d",
+    "url": "/goit-react-hw-04-reader/static/js/runtime~main.860f1cd1.js"
+  }
+]);
